63f83c6770872d77e00a8f1bfe9c9b9ff43994af
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2024-12-26 12:44:32 -0300

    Create _init_.py

---
+++ _init_.py
@@ -0,0 +1,10 @@
+"""
+Pacote contendo os módulos principais do jogo "Aventura no Labirinto".
+""" 
+aventura_no_labirinto/
+├── aventura_pkg/
+│   ├── __init__.py
+│   ├── labirinto.py
+│   ├── jogador.py
+│   └── utils.py
+├── main.py
